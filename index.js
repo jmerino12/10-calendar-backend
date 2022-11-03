6faaf25461591fd612f01ab13f64e2afdff2bced
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-03 18:32:50 -0500

    Creando rutas para el auth

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+require('dotenv').config();
+const app = express()
+
+app.use(express.static('public'))
+
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server corriendo en el puerto ${process.env.PORT}`)
+})
