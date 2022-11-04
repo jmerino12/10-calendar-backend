fd3bcf72999d1bdc48562c8d96b6e486453e5e5b
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-04 18:56:10 -0500

    Validaciones

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { obtenerEventos, crearNuevoEvento, actualizarEvento, eliminarEvento } = require('../controllers/events.controller');
+const { isDate } = require('../helpers/isDate');
 const { validarJWT } = require('../helpers/validat-jwt');
 const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
@@ -8,7 +9,13 @@ const router = Router();
 router.use(validarJWT)
 
 router.get('/', [validarCampos], obtenerEventos)
-router.post('/', [validarCampos], crearNuevoEvento)
+router.post('/',
+    [
+        check('title', 'titulo es obligatior').notEmpty(),
+        check('start', 'start es obligatior').custom(isDate),
+        check('end', 'end es obligatior').custom(isDate),
+        validarCampos
+    ], crearNuevoEvento)
 router.put('/:id', [validarCampos], actualizarEvento)
 router.delete('/:id', [validarCampos], eliminarEvento)
 
