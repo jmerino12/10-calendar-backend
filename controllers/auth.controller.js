276bc6154702562edc8dd9895647a6c5de0e2771
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-04 11:34:56 -0500

    Validaciones de usuario

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,13 +2,23 @@ const { response } = require('express')
 const Usuario = require('../models/Usuario')
 
 const crearUsuario = async (req, res = response) => {
+    const { email, password } = req.body
 
-    const usuario = new Usuario(req.body)
+
     try {
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ya existe ese correo',
+            })
+        }
+        usuario = new Usuario(req.body)
         await usuario.save();
         res.status(201).json({
             ok: true,
-            msg: 'Exitoso',
+            uid: usuario.id,
+            name: usuario.name
         })
     } catch (error) {
         res.status(500).json({
