d5dfd053ee8a0ffebe82ec6bd87c83db2e1f02a8
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-04 10:15:01 -0500

    Recuperar info de un posteo

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,29 @@
 const { response } = require('express')
 
 const crearUsuario = (req, res = response) => {
-    res.json({
+    const { name, email, password } = req.body
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'el nombre debe tener 5 letras'
+        })
+    }
+    return res.json({
         ok: true,
-        msg: 'registro'
+        msg: 'registro',
+        user: req.body
     })
 }
 
 const loginUsario = (req, res = response) => {
+    const { email, password } = req.body
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        login: {
+            email,
+            password
+        }
     })
 }
 
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 require('dotenv').config();
+
 const app = express()
 
 app.use(express.static('public'))
 
+app.use(express.json())
+
+
 app.use('/api/auth', require('./routes/auth'))
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server corriendo en el puerto ${process.env.PORT}`)
