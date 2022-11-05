75c8e01e882d4d8fdd904cea0a98df9665796da7
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-04 19:46:49 -0500

    Actualizar evento

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -32,11 +32,44 @@ const crearNuevoEvento = async (req, res = response) => {
 
 
 }
-const actualizarEvento = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizar evento'
-    })
+const actualizarEvento = async (req, res = response) => {
+    const eventoId = req.params.id
+    const uid = req.uid
+
+    try {
+        const evento = await Evento.findById(eventoId);
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: "Evento no existe con ese ID"
+            })
+        }
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No esta autorizado para hacer esto'
+            })
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+        res.status(200).json({
+            ok: true,
+            msg: 'Evento actualizado',
+            evento: eventoActualizado
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
 }
 const eliminarEvento = (req, res = response) => {
     res.json({
