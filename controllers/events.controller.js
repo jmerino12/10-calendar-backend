2db7e0ff4dbae2b229983bd5f3a8a9df106701c7
jonathan.merino <jonathan.merino@ceiba.com.co>
2022-11-04 19:36:08 -0500

    Obtener eventos

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -1,10 +1,12 @@
 const { response } = require('express')
 const Evento = require('../models/Evento')
 
-const obtenerEventos = (req, res = response) => {
+const obtenerEventos = async (req, res = response) => {
+
+    const eventos = await Evento.find().populate('user', 'name');
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
     })
 }
 
